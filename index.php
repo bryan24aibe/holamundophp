09e3d68a910d5de1c4ce543029293a1a77cd6ca3
balarai <b24ordonez@gmail.com>
2024-11-12 12:22:10 -0500

    feat(docker): add Dockerfile to run PHP application with Apache

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo PHP</title>
+</head>
+<body>
+
+    <h1><?php echo "Hola Mundo"; ?></h1>
+
+    <form method="post">
+        <button type="submit" name="deploy">Iniciar Deploy</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['deploy'])) {
+        echo "<p>Felicidades, iniciaste un deploy en PHP</p>";
+    }
+    ?>
+
+</body>
+</html>
